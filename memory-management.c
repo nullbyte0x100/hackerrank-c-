e75d6dc31395232244b474e3d650020e362bae5d
trevormalesike <101787383+trevormalesike@users.noreply.github.com>
2022-05-30 12:20:47 +0300

    Memory manament

---
+++ memory-management.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    char name[100];
+    char *descp;
+    strcpy(name,"Trevor");
+    if (descp==NULL){
+        fprintf("Error, unable to allocate memeory");
+
+    }else{
+        strcpy(descp,"That is my name");
+    }
+    printf("%s",descp);
+    return 0;
+
+}
