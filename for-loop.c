832c32fcf18a51eeacf36b736e2db560785a52b0
trevormalesike <101787383+trevormalesike@users.noreply.github.com>
2022-05-30 10:28:23 +0300

    Sum of five digits

--- for-loop.c
+++ for-loop.c
@@ -3,50 +3,51 @@
 #include <math.h>
 #include <stdlib.h>
 
-
-
-int main() 
+int main()
 {
     int a, b;
     scanf("%d\n%d", &a, &b);
-  	// Complete the code.
-    for (int i=a;i<=b;i++){
-        if (i>=1 && i<=9){
-            switch (i){
-		    case 1:
-			    printf("one");
-			    break;
-	        case 2:
-			    printf("two");
-			    break;
+    // Complete the code.
+    for (int i = a; i <= b; i++)
+    {
+        if (i >= 1 && i <= 9)
+        {
+            switch (i)
+            {
+            case 1:
+                printf("one\n");
+                break;
+            case 2:
+                printf("two\n");
+                break;
             case 3:
-                printf("three");
+                printf("three\n");
                 break;
             case 4:
-                printf("four");
+                printf("four\n");
                 break;
             case 5:
-                printf("five");
+                printf("five\n");
                 break;
             case 6:
-                printf("six");
+                printf("six\n");
                 break;
             case 7:
-                printf("seven");
+                printf("seven\n");
                 break;
             case 8:
-                printf("eight");
+                printf("eight\n");
                 break;
             case 9:
-                printf("nine");
+                printf("nine\n");
                 break;
-            default:
-                printf("Not found");
-                break;
-        }
+            }
+        }else if (i>9 && i%2==0){
+            printf("even\n");
+        }else{
+            printf("odd\n");
         }
     }
 
     return 0;
 }
-
---
+++ sum-of-five-digit.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+	int n,m;
+	int sum=0;
+
+	scanf("%d",&n);
+	while (n>0){
+		m=n%10;
+		sum+=m;
+		n?<F12
+	return 0;
+}
