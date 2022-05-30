79d083fbf6f0a5b346b71dac30735a5d966ecbd2
trevormalesike <101787383+trevormalesike@users.noreply.github.com>
2022-05-30 10:16:46 +0300

    Changed

--- for-loop.c
+++ for-loop.c
@@ -11,7 +11,40 @@ int main()
     scanf("%d\n%d", &a, &b);
   	// Complete the code.
     for (int i=a;i<=b;i++){
-        
+        if (i>=1 && i<=9){
+            switch (i){
+		    case 1:
+			    printf("one");
+			    break;
+	        case 2:
+			    printf("two");
+			    break;
+            case 3:
+                printf("three");
+                break;
+            case 4:
+                printf("four");
+                break;
+            case 5:
+                printf("five");
+                break;
+            case 6:
+                printf("six");
+                break;
+            case 7:
+                printf("seven");
+                break;
+            case 8:
+                printf("eight");
+                break;
+            case 9:
+                printf("nine");
+                break;
+            default:
+                printf("Not found");
+                break;
+        }
+        }
     }
 
     return 0;
