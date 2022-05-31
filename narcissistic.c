f5e3748ad368f88b06f48de1b8035d0885d0139c
trevormalesike <101787383+trevormalesike@users.noreply.github.com>
2022-05-31 09:28:44 +0300

    Changed back to C

--- narcissistic.c
+++ narcissistic.c
@@ -1,17 +1,13 @@
 #include<stdio.h>
 #include<stdbool.h>
-int narcissistic(int num)
+bool narcissistic(int num)
 {
     int m;
-    int sum=0;
     while (num>0){
         m=num%10;
-        sum+=m;
         num/=10;
 
     }
-    return sum;
 }
 int main(){
-    printf("The sum is %d",narcissistic(10564));
 }
