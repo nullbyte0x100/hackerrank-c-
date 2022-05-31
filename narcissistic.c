28a6c5d94eda5dc23d81c1ce042dd38f3d2db4ec
trevormalesike <101787383+trevormalesike@users.noreply.github.com>
2022-05-31 09:37:55 +0300

    Okay time to test it out

--- narcissistic.c
+++ narcissistic.c
@@ -1,13 +1,21 @@
 #include<stdio.h>
 #include<stdbool.h>
+#include<math.h>
 bool narcissistic(int num)
 {
-    int m;
+    int sum=0;
     while (num>0){
-        m=num%10;
+        int mod=num%10;
+        int ans=pow(mod,3);
+        sum+=ans;
         num/=10;
-
+    }
+    if (sum==num){
+        return true;
+    }else{
+        return false;
     }
 }
 int main(){
+    printf("%d\n",narcissistic(153));
 }
